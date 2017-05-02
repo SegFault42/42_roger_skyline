945b63d81a37ff2d2f0f2ff0d2b0b3970b245056
SegFault42 <rabougue@student.42.fr>
2017-05-02 23:41:15 +0200

    added mail

--- Scripting/04.sh
+++ Scripting/04.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-if [[ $(md5sum /var/spool/cron/crontabs/root | cut -d' ' -f1) == $(md5sum /etc/crontab | cut -d' ' -f1) ]]
-then
-    # send your notification
+value1="$(md5sum /var/spool/cron/crontabs/root.backup | cut -d' ' -f1)"
+value2="$(md5sum /var/spool/cron/crontabs/root | cut -d' ' -f1)"
+
+if [ "$value1" != "$value2" ]; then
+	ssmtp root < /home/rabougue/Documents/42_roger_skyline/Scripting/mail
 fi
+
+cp /var/spool/cron/crontabs/root /var/spool/cron/crontabs/root.backup
+
+#crontab -e
+# ajouter a la fin : 00 00 * * * /home/rabougue/Documents/42_roger_skyline/Scripting/04.sh
