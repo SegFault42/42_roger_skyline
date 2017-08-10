32da8e1e1a55acb8a9a73b2ca8e056da0c3dc95f
root <root@debian>
2017-08-09 21:59:48 -0400

    04.sh corrected

--- Scripting/04.sh
+++ Scripting/04.sh
@@ -4,10 +4,10 @@ value1="$(md5sum /var/spool/cron/crontabs/root.backup | cut -d' ' -f1)"
 value2="$(md5sum /var/spool/cron/crontabs/root | cut -d' ' -f1)"
 
 if [ "$value1" != "$value2" ]; then
-	/usr/sbin/ssmtp root < /home/rabougue/Documents/42_roger_skyline/Scripting/mail
+	mail -s test root < /root/Documents/42_roger_skyline/Scripting/mail
 fi
 
 cp /var/spool/cron/crontabs/root /var/spool/cron/crontabs/root.backup
 
 #crontab -e
-# ajouter a la fin : 00 00 * * * /home/rabougue/Documents/42_roger_skyline/Scripting/04.sh
+# ajouter a la fin : 00 00 * * * /root/Documents/42_roger_skyline/Scripting/04.sh
