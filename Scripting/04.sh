44cfbe40eb6a5fa19799c7a64588697dd7bc80f1
root <root@domain.tld>
2017-10-26 10:15:57 -0400

    finish

--- Scripting/04.sh
+++ Scripting/04.sh
@@ -4,7 +4,7 @@ value1="$(md5sum /var/spool/cron/crontabs/root.backup | cut -d' ' -f1)"
 value2="$(md5sum /var/spool/cron/crontabs/root | cut -d' ' -f1)"
 
 if [ "$value1" != "$value2" ]; then
-	mail -s test root < /root/Documents/42_roger_skyline/Scripting/mail
+	ssmtp ramzi90000@gmail.com < mail
 fi
 
 cp /var/spool/cron/crontabs/root /var/spool/cron/crontabs/root.backup
